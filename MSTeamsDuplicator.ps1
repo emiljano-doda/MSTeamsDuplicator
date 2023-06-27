39f8149354808d8a49c3c45d380842966634fb27
Emiljano Doda <115354251+emiljano-doda@users.noreply.github.com>
2023-06-27 19:37:40 +0200

    Add files via upload

--- MSTeamsDuplicator.ps1
+++ MSTeamsDuplicator.ps1
@@ -5,7 +5,7 @@ if([System.IO.Directory]::Exists($FolderName))
     # Uses the file name as the profile name
     $MSTEAMS_PROFILE = 'NewProfile'
 
-    Write-Host "- Using profile '$MSTEAMS_PROFILE'"
+    Write-Host "Using profile '$MSTEAMS_PROFILE'" -ForegroundColor Green
 
     # Set the custom profile path
     $USERPROFILE = Join-Path $env:LOCALAPPDATA "Microsoft\Teams\CustomProfiles\$MSTEAMS_PROFILE"
@@ -14,7 +14,7 @@ if([System.IO.Directory]::Exists($FolderName))
     $OLD_USERPROFILE = $env:USERPROFILE
 
     # Launch MS Teams with the custom profile
-    Write-Host "- Launching MS Teams with profile '$MSTEAMS_PROFILE'"
+    Write-Host "Launching another Teams instance with profile named : '$MSTEAMS_PROFILE'" -ForegroundColor Green
     Set-Location "$OLD_USERPROFILE\AppData\Local\Microsoft\Teams"
 
     $teamsProcessStartInfo = New-Object System.Diagnostics.ProcessStartInfo
@@ -31,12 +31,12 @@ if([System.IO.Directory]::Exists($FolderName))
     $FolderName = "$env:LOCALAPPDATA\Microsoft\Teams\CustomProfiles\$MSTEAMS_PROFILE\Desktop"
     if([System.IO.Directory]::Exists($FolderName))
     {
-        Write-Host "- Folder Already Exists"
+        Write-Host "Folder Already Exists" -ForegroundColor Green
         Get-ChildItem -Path $FolderName | Where-Object {$_.CreationTime -gt (Get-Date).Date}  
     }
     else
     {
-        Write-Host "- Folder Doesn't Exists , Creating it"
+        Write-Host "Folder Doesn't Exists , Creating it" -ForegroundColor Green
         New-Item $FolderName -ItemType Directory
     }
 
@@ -45,5 +45,6 @@ if([System.IO.Directory]::Exists($FolderName))
 }
 else
 {
-    Write-Host "Error : It seems Teams isn't installed in the default path : ..\AppData\Microsoft\Teams\"
+    Write-Host "Error : It seems Teams isn't installed in the default path : ..\AppData\Microsoft\Teams\" -ForegroundColor Red
+    Write-Host "Aborted : Make sure to install Teams !" -ForegroundColor Red
 }
